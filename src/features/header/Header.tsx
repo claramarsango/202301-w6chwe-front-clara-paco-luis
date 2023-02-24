020ae72d875169d906a0bcd86ec346b04c5dfedd
claramarsango <claraxmarsango@gmail.com>
2023-02-24 16:04:01 +0100

    add link of title to home page

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header-container">
-      <img
-        src="assets/robot-logo.png"
-        alt="robot logo"
-        className="header__logo"
-      />
-      <h1 className="header__title">PLC Robotics</h1>
+      <Link to="/" className="header__link">
+        <img
+          src="assets/robot-logo.png"
+          alt="robot logo"
+          className="header__logo"
+        />
+        <h1 className="header__title">PLC Robotics</h1>
+      </Link>
     </header>
   );
 };
