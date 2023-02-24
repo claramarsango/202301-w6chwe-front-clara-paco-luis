f581db1ddc0d0b5690b0903f70b180e48cc48156
claramarsango <claraxmarsango@gmail.com>
2023-02-24 16:04:55 +0100

    add header test with memory router

--- src/features/header/Header.test.tsx
+++ src/features/header/Header.test.tsx
@@ -1,1 +1,15 @@
-export {};
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Header from "./Header";
+
+describe("Given a header component", () => {
+  test("When the page is rendered, there should be a title", () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    const title = screen.getByRole("heading");
+    expect(title).toBeInTheDocument();
+  });
+});
