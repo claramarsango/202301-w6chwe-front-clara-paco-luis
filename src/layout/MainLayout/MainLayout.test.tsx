f642e96be379ae3bdf1d3f8d528709778733f685
claramarsango <claraxmarsango@gmail.com>
2023-02-24 16:05:18 +0100

    add memory router to main layout test

--- src/layout/MainLayout/MainLayout.test.tsx
+++ src/layout/MainLayout/MainLayout.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import MainLayout from "./MainLayout";
 
 describe("Given a main layout", () => {
   test("When a page is accessed, then return a title", () => {
-    render(<MainLayout />);
+    render(
+      <MemoryRouter>
+        <MainLayout />
+      </MemoryRouter>
+    );
     const title = screen.getByRole("heading");
     expect(title).toBeInTheDocument();
   });
