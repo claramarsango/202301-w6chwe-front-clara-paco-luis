5c640201d14a6f86006a7e30cdb5adbdac62ad7f
claramarsango <claraxmarsango@gmail.com>
2023-02-26 22:33:31 +0100

    add provider to home page test

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,10 +1,16 @@
-import { render, screen } from "@testing-library/react";
-import Home from "./Home";
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
+import Home from './Home';
 
-describe("Given a home page", () => {
-  test("When there is a title, then render it on the page", () => {
-    render(<Home />);
-    const title = screen.getByRole("heading");
+describe('Given a home page', () => {
+  test('When there is a title, then render it on the page', () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>,
+    );
+    const title = screen.getByRole('heading');
     expect(title).toBeInTheDocument();
   });
 });
